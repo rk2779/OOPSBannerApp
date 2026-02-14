b6304dce9e82ef9ab22c3cb26378041fe6a372fc
rk2779 <rk2779@srmist.edu.in>
2026-02-14 17:00:56 +0530

    rk2779 Added:UC2- 7-line OOPS banner

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,9 +1,11 @@
 public class OOPSBanner {
-
     public static void main(String[] args) {
-
-        System.out.println("Welcome to OOPSBanner application");
-
+        System.out.println("    ***        ***     ******       *****       ");
+		System.out.println("  **   **    **   **   **    **   **           " );
+        System.out.println(" **     **  **     **  **     **  **      ");
+        System.out.println(" **     **  **     **  **    **	    ***" );
+        System.out.println(" **     **  **     **  ******          **           ");
+		System.out.println("  **   **    **   **   **              **		");
+        System.out.println("    ***        ***     **         *****         ");
     }
-
 }
