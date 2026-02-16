2ecb57137a7e7e50a6a5f325ea4e93513e33754c
rk2779 <rk2779@srmist.edu.in>
2026-02-16 13:47:47 +0530

    rk2779 Added: UC3 - Refactored with String.join for efficiency

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,9 +1,11 @@
 public class OOPSBanner {
-
     public static void main(String[] args) {
-
-        System.out.println("Welcome to OOPSBanner application");
-
+        System.out.println(String.join(" ","   ***   ","   ***   ","******   "," *****"));
+		System.out.println(String.join(" "," **   ** "," **   ** ","**    ** ","**    "));
+        System.out.println(String.join(" ","**     **","**     **","**     **","**    "));
+        System.out.println(String.join(" ","**     **","**     **","**    ** "," ***  "));
+        System.out.println(String.join(" ","**     **","**     **","******   ","    **"));
+		System.out.println(String.join(" "," **   ** "," **   ** ","**       ","    **"));
+        System.out.println(String.join(" ","   ***   ","   ***   ","**       ","***** "));
     }
-
 }
