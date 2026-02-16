882341d9a8f5c0d7e37fb34df9618c160b39de80
rk2779 <rk2779@srmist.edu.in>
2026-02-16 13:03:38 +0530

    rk2779 Added :OOPSBanner main application

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,11 +1,9 @@
 public class OOPSBanner {
+
     public static void main(String[] args) {
-        System.out.println("    ***        ***     ******       *****       ");
-		System.out.println("  **   **    **   **   **    **   **           " );
-        System.out.println(" **     **  **     **  **     **  **      ");
-        System.out.println(" **     **  **     **  **    **	    ***" );
-        System.out.println(" **     **  **     **  ******          **           ");
-		System.out.println("  **   **    **   **   **              **		");
-        System.out.println("    ***        ***     **         *****         ");
+
+        System.out.println("Welcome to OOPSBanner application");
+
     }
+
 }
